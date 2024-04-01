aec72b5012e8418e15b15b1a8ef67e75e79ec948
I Ketut Mahendra <63593289+mahendraketut@users.noreply.github.com>
2024-04-01 11:35:24 +0800

    Update User model to use soft deletes and modify users table
    The User model has been updated to use the SoftDeletes trait, adding a "deleted\_at" column to the users table. The "name" column in the users table has been changed to "full\_name" and a new "username" column has been added, which is unique.
    
    The API routes have been updated to include a version prefix (v1) and to include a test route and authentication routes for registration, login, and logout.

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Requests\StoreLoginRequest;
+use App\Traits\ApiResponseTrait;
+
+class LoginController extends Controller
+{
+    use ApiResponseTrait;
+    /**
+     * Handle an incoming authentication request.
+     *
+     * @param StoreLoginRequest $request
+     */
+    public function login(StoreLoginRequest $request)
+    {
+        // Check if the user credentials are valid
+        if (!auth()->attempt($request->only('email', 'password'))) {
+            return $this->unauthorizedResponse('Invalid credentials');
+        }
+
+        // Get the authenticated user
+        $user = User::where('email', $request->email)->firstOrFail();
+
+        // Generate a token for the user
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        // Return the user and token details with response code 200 - OK
+        return $this->successResponse([
+            'user' => $user,
+            'token' => $token,
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('v1')->group(function () {
+    Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    //Route test connection
+    Route::get('test', function () {
+        return response()->json(['message' => 'API is working']);
+    });
+
+    Route::post('register', 'App\Http\Controllers\Auth\RegisterController@register');
+    Route::post('login', 'App\Http\Controllers\Auth\LoginController@login');
+
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('logout', 'App\Http\Controllers\Auth\LogoutController@logout');
+    });
 });
