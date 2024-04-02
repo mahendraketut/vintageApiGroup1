03d3adf38a88efa67aae2153cf7f82b48083d133
Indira <indira6166@gmail.com>
2024-04-02 08:50:17 +0700

    working on product request

---
+++ app/Rules/NoNegativeValue.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Rules;
+
+use Closure;
+use Illuminate\Contracts\Validation\Rule;
+
+class NoNegativeValue implements Rule
+{
+    /**
+     * Run the validation rule.
+     *
+     * @param  \Closure(string): \Illuminate\Translation\PotentiallyTranslatedString  $fail
+     */
+    public function passes($attribute,$value)
+    {
+        return $value >= 0;
+    }
+
+    public function message()
+    {
+        return "The :attribute can't be a negative value";
+    }
+}
