8ff3b5b601d12f5cbede66cb1478db29d044cde0
Indira <indira6166@gmail.com>
2024-04-06 15:24:51 +0700

    wishlist done

---
+++ app/Models/Wishlist.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Wishlist extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['product_id', 'user_id'];
+
+    public function product():BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function user():BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,7 @@
 <?php
 
-use App\Http\Controllers\BrandController;
-use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
+use App\Models\Wishlist;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,13 +16,31 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::prefix('v1')->group(function () {
-    Route::resource('/brands', BrandController::class);
-    Route::resource('/categories', CategoryController::class);
-    Route::put('/products/restore', ProductController::class, 'restore');
-    Route::resource('/products', ProductController::class);
+    Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    //Route test connection
+    Route::get('test', function () {
+        return response()->json(['message' => 'API is working']);
+    });
+
+    Route::post('register', 'App\Http\Controllers\Auth\RegisterController@register');
+    Route::post('login', 'App\Http\Controllers\Auth\LoginController@login');
+
+    Route::get('categories', 'App\Http\Controllers\CategoryController@index');
+    Route::get('brands', 'App\Http\Controllers\BrandController@index');
+
+    Route::get('products', 'App\Http\Controllers\ProductController@index');
+
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::put('change-password', 'App\Http\Controllers\Auth\ChangePasswordController@changePassword');
+        Route::post('logout', 'App\Http\Controllers\Auth\LogoutController@logout');
+
+        Route::put('/products/restore', 'App\Http\Controllers\ProductController@restore');
+        Route::resource('/products', 'App\Http\Controllers\ProductController');
+
+        Route::resource('wishlists', 'App\Http\Controllers\WishlistController');
+    });
 });
