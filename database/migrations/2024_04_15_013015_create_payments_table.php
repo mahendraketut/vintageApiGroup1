23a33a595db5bec0dc4215a23319d7054474ff83
Indira <indira6166@gmail.com>
2024-04-16 08:12:28 +0700

    creating payment feature

---
+++ database/migrations/2024_04_15_013015_create_payments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('method_id');
+            $table->unsignedBigInteger('order_id');
+            $table->decimal('amount');
+            $table->timestamps();
+        });
+
+        Schema::create('payment_methods', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payments');
+        Schema::dropIfExists('payment_methods');
+    }
+};
